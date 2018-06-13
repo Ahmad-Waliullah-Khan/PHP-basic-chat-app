d7de8b90f76cadb21c8990cd2ce4df10baa16d2d
Oli <oli.de.psycho.gamer@gmail.com>
2018-06-13 15:35:25 +0530

    Initial push commit

---
+++ shoutit.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 08, 2017 at 07:04 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shoutit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shouts`
+--
+
+CREATE TABLE `shouts` (
+  `id` int(11) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `shouts`
+--
+
+INSERT INTO `shouts` (`id`, `user`, `message`, `time`) VALUES
+(1, 'John', 'Hello guys', '11:00:00'),
+(2, 'Brad', 'Hello', '11:05:02'),
+(3, 'John', 'Hello guys', '11:00:00'),
+(4, 'Brad', 'Hello', '11:05:02'),
+(5, 'Oli', 'Hi', '11:44:42'),
+(6, 'Oli', 'the database seems to be working correctly', '11:49:26'),
+(7, 'Jameelah', 'Nice shoutbox! :)', '07:40:06');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shouts`
+--
+ALTER TABLE `shouts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shouts`
+--
+ALTER TABLE `shouts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
